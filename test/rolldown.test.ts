9c5ea86127509eb58b3f31b09b84887d8f5c9c54
Lucas Nørgård <lucasnrgaard@gmail.com>
2025-05-29 07:37:19 +0200

    chore: remove leftover cleanup

--- test/rolldown.test.ts
+++ test/rolldown.test.ts
@@ -8,9 +8,7 @@ import { removeComments } from "./utils";
 
 describe("rolldown", () => {
   it("expect toml import to be a json object", async () => {
-    const testdirPath = await testdir.from(join(import.meta.dirname, "fixtures/basic"), {
-      cleanup: false,
-    });
+    const testdirPath = await testdir.from(join(import.meta.dirname, "fixtures/basic"));
 
     expect(testdirPath).toBeDefined();
 
