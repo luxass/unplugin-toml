c538e6d124fea4723b5cae3de33eab7da345d0dd
Lucas Nørgård <lucasnrgaard@gmail.com>
2025-05-29 07:35:50 +0200

    refactor: enhance rolldown tests by adding cleanup option and using dedent for string comparison

--- test/rolldown.test.ts
+++ test/rolldown.test.ts
@@ -1,4 +1,5 @@
 import { join } from "node:path";
+import { dedent } from "@luxass/utils";
 import { rolldown } from "rolldown";
 import { describe, expect, it } from "vitest";
 import { testdir } from "vitest-testdirs";
@@ -7,7 +8,9 @@ import { removeComments } from "./utils";
 
 describe("rolldown", () => {
   it("expect toml import to be a json object", async () => {
-    const testdirPath = await testdir.from(join(import.meta.dirname, "fixtures/basic"));
+    const testdirPath = await testdir.from(join(import.meta.dirname, "fixtures/basic"), {
+      cleanup: false,
+    });
 
     expect(testdirPath).toBeDefined();
 
@@ -17,12 +20,21 @@ describe("rolldown", () => {
         TOMLPlugin(),
       ],
     });
-    const { output } = await bundle.generate({
+
+    await bundle.write({
+      dir: join(testdirPath, "dist"),
       format: "esm",
       sourcemap: false,
     });
 
-    expect(removeComments(output[0].code)).toMatchSnapshot();
+    const config = await import(join(testdirPath, "dist/basic.js")).then((m) => m.config);
+    expect(config).toBeDefined();
+
+    expect(config).toEqual({
+      pluginDir: "./plugins",
+      web: { enabled: true },
+      logging: { type: "stdout", level: "info" },
+    });
   });
 
   it("expect toml import to be a string", async () => {
@@ -36,12 +48,26 @@ describe("rolldown", () => {
         TOMLPlugin(),
       ],
     });
-    const { output } = await bundle.generate({
+
+    await bundle.write({
+      dir: join(testdirPath, "dist"),
       format: "esm",
       sourcemap: false,
     });
 
-    expect(removeComments(output[0].code)).toMatchSnapshot();
+    const config = await import(join(testdirPath, "dist/basic-raw.js")).then((m) => m.config);
+    expect(config).toBeDefined();
+
+    expect(config).toMatch(dedent`
+      pluginDir = "./plugins"
+
+      [web]
+      enabled = true
+
+      [logging]
+      type = "stdout"
+      level = "info"
+    `);
   });
 
   it("handle transforms", async () => {
@@ -63,11 +89,18 @@ describe("rolldown", () => {
         }),
       ],
     });
-    const { output } = await bundle.generate({
+
+    await bundle.write({
+      dir: join(testdirPath, "dist"),
       format: "esm",
       sourcemap: false,
     });
 
-    expect(removeComments(output[0].code)).toMatchSnapshot();
+    const config = await import(join(testdirPath, "dist/transform.js")).then((m) => m.config);
+    expect(config).toBeDefined();
+
+    expect(config).toEqual({
+      this: "transformed",
+    });
   });
 });
