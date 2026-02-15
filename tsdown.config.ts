8f64231013878f59a85866e13ecc0f74a04148ed
Lucas <lucasnrgaard@gmail.com>
2026-02-15 12:35:52 +0100

    feat!: drop support for esm

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,12 +2,21 @@ import { defineConfig } from "tsdown";
 
 export default defineConfig({
   entry: ["src/*.ts"],
-  format: ["cjs", "esm"],
+  format: ["esm"],
   clean: true,
   dts: true,
   treeshake: true,
   publint: true,
   outputOptions: {
     exports: "named",
+  },
+  exports: {
+    packageJson: false,
+    customExports(exports) {
+      exports["./types"] = { types: "./yaml.d.ts" };
+      exports["./package.json"] = "./package.json";
+
+      return exports;
+    },
   },
 });
