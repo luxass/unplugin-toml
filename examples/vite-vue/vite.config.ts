b98aa84d5529c68bd0beb3da29005da89949e96a
Lucas <lucasnrgaard@gmail.com>
2026-03-27 19:00:07 +0100

    chore(vite-vue): reorder imports in vite.config.ts for consistency

--- examples/vite-vue/vite.config.ts
+++ examples/vite-vue/vite.config.ts
@@ -1,19 +1,19 @@
 import tailwindcss from "@tailwindcss/vite";
-import Vue from "@vitejs/plugin-vue";
-import TomlPlugin from "unplugin-toml/vite";
-import VueRouter from "unplugin-vue-router/vite";
+import vue from "@vitejs/plugin-vue";
+import toml from "unplugin-toml/vite";
+import vueRouter from "unplugin-vue-router/vite";
 import { defineConfig } from "vite";
-import Inspect from "vite-plugin-inspect";
-import VueDevTools from "vite-plugin-vue-devtools";
+import inspect from "vite-plugin-inspect";
+import vueDevtools from "vite-plugin-vue-devtools";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    VueRouter(),
-    Vue(),
-    VueDevTools(),
-    TomlPlugin(),
-    Inspect(),
+    vueRouter(),
+    vue(),
+    vueDevtools(),
+    toml(),
+    inspect(),
     tailwindcss(),
   ],
 });
